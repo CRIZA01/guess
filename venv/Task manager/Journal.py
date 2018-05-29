80c78d9e49394fffc7e4d266b24962a30fcdcca3
CRIZA01 <chetan.rizal@stu.jeffersons.kyschools.us>
2018-05-29 08:39:11 -0400

    final submission

---
+++ venv/Task manager/Journal.py
@@ -0,0 +1,13 @@
+while True:
+    favorite_day = input("what is your favorite day in this week?")
+
+    if favorite_day == "friday":
+        print("that's  my favorite day too...")
+    elif favorite_day == "sunday":
+        print("i don't like sunday")
+    elif favorite_day == "Saturday":
+        print("what")
+    elif favorite_day == "saturday":
+        print(" like it too")
+
+
